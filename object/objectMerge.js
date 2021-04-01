1aaaca94214fb29ccd2664eaf58abec66f5f23af
Xi Luo <xiluo@XideMacBook-Air.local>
2021-03-31 22:40:04 -0400

    second

--- object/objectMerge.js
+++ object/objectMerge.js
@@ -7,3 +7,23 @@
     合并后: 
         { a: [ { x: 2 }, { y: 4 }, { z: 3 } ], b: [ 1, 2, 3 ], c: 'foo' }
 */
+import { concat } from "../array/concat";
+export function mergeObject(...objs) {
+  const result = {};
+
+  objs.forEach((obj) => {
+    Object.keys(obj).forEach((key) => {
+      const value = obj[key];
+      // result中没有key属性
+      if (!result.hasOwnProperty(key)) {
+        result[key] = value;
+      } else {
+        // result中有key属性
+        // 对原值与value进行合并生成新的数组
+        result[key] = concat([], result[key], value);
+      }
+    });
+  });
+
+  return result;
+}
